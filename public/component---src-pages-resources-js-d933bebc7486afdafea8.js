9ee32c814e3b2df6b0c2b848a7992540a2b775f3
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2019-10-11 19:42:54 -0400

    yarn upgrade and remove build folder cuz idk

--- public/component---src-pages-resources-js-d933bebc7486afdafea8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{302:function(e,t,n){"use strict";n.r(t);var a=n(0),o=n.n(a),s=n(294);t.default=function(e){return o.a.createElement(s.a,{location:e.location.pathname},o.a.createElement("h1",null,"NOT FOUND"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-resources-js-d933bebc7486afdafea8.js.map
